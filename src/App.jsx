f6b892012c08aeb93830098568644c4d94f79d61
adriano-r <adrianorosa9974@gmail.com>
2021-12-16 23:25:04 -0300

    adding dynamic active class

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
+import "./app.scss";
+import { useState } from "react";
 import Work from "./components/work/Work";
+import Menu from "./components/menu/Menu";
 import Intro from "./components/intro/Intro";
 import Topbar from "./components/topbar/Topbar";
 import Contact from "./components/contact/Contact";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
-import "./app.scss";
 
 function App() {
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <div className="app">
-            <Topbar />
+            <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
             <div className="sections">
                 <Intro />
                 <Portfolio />
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,64 @@
 import "./portfolio.scss";
+import { useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 
 export default function Portfolio() {
-    return <div className="portfolio" id="portfolio"></div>;
+    const [selected, setSelected] = useState("featured");
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        {
+            id: "web",
+            title: "Web App",
+        },
+        {
+            id: "mobile",
+            title: "Mobile App",
+        },
+        {
+            id: "design",
+            title: "Design",
+        },
+        {
+            id: "Content",
+            title: "Content",
+        },
+    ];
+
+    return (
+        <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((item) => {
+                    <PortfolioList
+                        title={item.title}
+                        active={selected === item.id}
+                        setSelected={setSelected}
+                        id={item.id}
+                    />;
+                })}
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/crypto.jpeg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/crypto.jpeg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/crypto.jpeg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img src="assets/crypto.jpeg" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
+        </div>
+    );
 }
